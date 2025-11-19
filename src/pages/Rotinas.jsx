9544461b3b5351ef0616cfea6656c39e2a83d9f1
Samuel <samuel.scv14@gmail.com>
2025-11-19 14:32:22 -0300

    gf

--- src/pages/Rotinas.jsx
+++ src/pages/Rotinas.jsx
@@ -2,33 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Rotinas() {
-
   const buttonStyle =
-    'p-8 bg-[#374D77] rounded-2xl border-none transition-colors duration-300 hover:bg-[#253450] text-white text-[30px] font-normal cursor-pointer';
+    'p-6 lg:p-8 bg-[#374D77] rounded-xl lg:rounded-2xl border-none transition-colors  hover:bg-[#253450] text-white text-lg lg:text-[30px] font-normal cursor-pointe text-center  ';
 
   return (
-    <div className="min-h-screen bg-[#182132] text-white font-[Inter] antialiased">
-      <header className="pt-5 flex justify-between items-center px-8 py-4">
-        <Link to="/dashboard" className="flex items-center no-underline text-white">
-                  <img src="/LOGONEURO.png" alt="NeuroPlanner Logo" className="w-12 mr-3" />
-                  <h1 className="text-2xl md:text-3xl font-bold">
-                    <span className="text-[#30BBDE]">Neuro</span>Planner
-                  </h1>
-                </Link>
+    <div className="min-h-screen bg-[#182132] text-white font-[Inter] px-4 lg:px-0">
+      <header className="pt-5 flex justify-between items-center px-4 lg:px-8 py-4">
+        <Link to="/dashboard" className="flex items-center text-white">
+          <img src="/LOGONEURO.png" alt="NeuroPlanner Logo" className="w-8 lg:w-12 mr-2" />
+          <h1 className="text-xl lg:text-3xl font-bold">
+            <span className="text-[#30BBDE]">Neuro</span>Planner
+          </h1>
+        </Link>
         <div className="button">
-          <button className="bg-transparent border-none">
-            <Link to="/dashboard" className="text-white text-2xl no-underline">
+          <button>
+            <Link to="/dashboard" className="text-white text-base lg:text-2xl no-underline">
               Voltar
             </Link>
           </button>
         </div>
       </header>
 
-      <main className="flex justify-center items-center flex-col mt-36">
-        <div>
-          <p className="text-6xl font-normal">CRIADOR DE ROTINA</p>
+      <main className="flex justify-center items-center flex-col mt-50 lg:mt-36 px-4">
+        <div className="text-center">
+          <p className="text-2xl lg:text-6xl font-normal">CRIADOR DE ROTINA</p>
         </div>
-        <div className="mt-16 flex flex-col space-y-8">
+        <div className="mt-8 lg:mt-16 flex flex-col gap-4 lg:gap-8 w-full max-w-xl">
           <button className={buttonStyle}>
             <p>CRIAR NOVA ROTINA</p>
           </button>
